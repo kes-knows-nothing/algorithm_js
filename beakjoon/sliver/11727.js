e4b73f50444fee870a258961cbe8d259332d5918
kes-knows-nothing <allinpositive@gmail.com>
2023-09-19 05:53:25 +0900

    11727 2xn 타일링 2

---
+++ beakjoon/sliver/11727.js
@@ -0,0 +1,14 @@
+const input = require("fs").readFileSync("/dev/stdin").toString();
+
+const num = Number(input);
+
+const DP = new Array(num + 1).fill(0);
+DP[0] = 1;
+DP[1] = 1;
+DP[2] = 3;
+
+for (let i = 2; i <= num; i++) {
+  DP[i] = (DP[i - 1] + DP[i - 2] * 2) % 10007;
+}
+
+console.log(DP[num]);
