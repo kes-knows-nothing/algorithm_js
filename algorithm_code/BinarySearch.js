2c19b8a0114ddb899dc36a02699f6f2721bdc422
kes-knows-nothing <allinpositive@gmail.com>
2023-09-18 12:28:46 +0900

    이진탐색 하한선 상한선 함수

--- algorithm_code/BinarySearch.js
+++ algorithm_code/BinarySearch.js
@@ -16,3 +16,23 @@ arr = [1, 3, 5, 7, 9, 13, 15, 17, 19];
 let result = binarySearch(arr, target, 0, n - 1);
 if (result == -1) console.log("원소가 존재하지 않습니다.");
 else console.log(`${result + 1}번째 원소입니다.`);
+
+// 하안선과 상한선 함수
+
+function lowerBound(arr, target, start, end) {
+  while (start < end) {
+    let mid = parseInt((start + end) / 2);
+    if (arr[mid] >= target) end = mid;
+    else start = mid + 1;
+  }
+  return end;
+}
+
+function upperBound(arr, target, start, end) {
+  while (start < end) {
+    let mid = parseInt((start + end) / 2);
+    if (arr[mid] > target) end = mid;
+    else start = mid + 1;
+  }
+  return end;
+}
