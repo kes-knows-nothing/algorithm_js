d982f1f9c1950aa3425750ca3f98e1843f25ccb3
kes-knows-nothing <allinpositive@gmail.com>
2023-09-22 12:29:12 +0900

    11052 카드 구매하기

--- beakjoon/sliver/11052.js
+++ beakjoon/sliver/11052.js
@@ -1,6 +1,23 @@
-const [n, input] = require('fs').readFileSync("example.txt").toString().trim().split('\n');
-const inputArr = input.trim().split(" ")
-let answer;
-for (let i = 0; i < n + 1; i++) {
-    
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .replaceAll("\r", "")
+  .split("\n");
+
+const n = Number(input[0]);
+const price = input[1].split(" ").map(Number);
+price.unshift(0);
+
+let DP = new Array(n + 1).fill(0);
+DP[0] = 0;
+DP[1] = Number(price[1]);
+
+for (let i = 2; i <= n; i++) {
+  let findmax = [];
+  for (let j = 1; j < i; j++) {
+    findmax.push(DP[i - j] + price[j]);
+  }
+  findmax.push(price[i]);
+  DP[i] = Math.max(...findmax);
 }
+console.log(DP[n]);
