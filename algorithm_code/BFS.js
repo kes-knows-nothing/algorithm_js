564c1b5093ea1487ba07e4a1dc2191546a95e32c
Kes <epik1219@naver.com>
2023-10-05 14:40:33 +0900

    MC

--- algorithm_code/BFS.js
+++ algorithm_code/BFS.js
@@ -1,1 +1,41 @@
 // Breadth-First Search
+
+class Queue {
+  constructor() {
+    this.item = {};
+    this.headIndex = 0;
+    this.tailIndex = 0;
+  }
+  enqueue(item) {
+    this.items[this.tailIndex] = item;
+    this.tailIndex++;
+  }
+  dequeue() {
+    const item = this.items[this.headIndex];
+    delete this.items[this.headIndex];
+    this.headIndex++;
+    return item;
+  }
+  peek() {
+    return this.items[this.headIndex];
+  }
+  getLength() {
+    return this.tailIndex - this.headIndex;
+  }
+}
+
+function bfs(graph, start, visited) {
+  queue = new Queue();
+  queue = enqueue(start);
+  visited[start] = true;
+  while (queue.getLength() != 0) {
+    v = queue.dequeue();
+    console.log(v);
+    for (i of graph[v]) {
+      if (!visited[i]) {
+        queue.enqueue(i);
+        visited[i] = ture;
+      }
+    }
+  }
+}
